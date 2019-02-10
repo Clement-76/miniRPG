dcd4edb511d484bedd0ab44b46219ddd9f0724d0
Clement-76 <clement.patigny@gmail.com>
2019-02-10 12:37:17 +0100

    adding the setUser method to add a new user to the db

---
+++ model/UserManager.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ClementPatigny\Model;
+
+class UserManager extends Manager {
+    /**
+     * add a new user to the db
+     * @param $userFeatures
+     * @throws \Exception
+     */
+    public function setUser($userFeatures) {
+        $db = $this->connectDb();
+
+        try {
+            $q = $db->prepare('INSERT INTO users(pseudo, email, password, confirmation_key) VALUE(:pseudo, :email, :password, :confirmation_key)');
+
+            $q->bindValue(':pseudo', $userFeatures['pseudo'], \PDO::PARAM_STR);
+            $q->bindValue(':email', $userFeatures['email'], \PDO::PARAM_STR);
+            $q->bindValue(':password', $userFeatures['password'], \PDO::PARAM_STR);
+            $q->bindValue(':confirmation_key', $userFeatures['confirmationKey'], \PDO::PARAM_INT);
+
+            $q->execute();
+        } catch (Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+}
