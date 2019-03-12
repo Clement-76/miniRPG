0c2709239e9a61a1b559915c181c793bf558d3dc
Clement-76 <clement.patigny@gmail.com>
2019-03-12 21:17:00 +0100

    adding the possibility of move windows, open/close and bring forward

---
+++ assets/js/WindowRPG.js
@@ -0,0 +1,70 @@
+class WindowRPG {
+    constructor(windowClass, contentId) {
+        this.windowClass = windowClass;
+        this.window = $(`.window.${windowClass}`);
+        this.openBtn = $('#' + this.windowClass);
+        this.closeBtn = $(`.${windowClass} .icon-close`);
+        this.windowTopBar = $(`.${windowClass} > h2`);
+        this.content = $('#' + contentId);
+
+        this.openBtn.on('click', this.open.bind(this));
+        this.closeBtn.on('click', this.close.bind(this));
+        this.windowTopBar.on('mousedown', this.grab.bind(this));
+        this.window.on('mousedown', this.bringForward.bind(this));
+    }
+
+    bringForward() {
+        $('.window').each((i, elt) => {
+            let windowClass = elt.className.replace('window', '');
+            $('#' + windowClass).removeClass('active');
+
+            if ($(elt).css('z-index') > 0) {
+                $(elt).css('z-index', parseFloat($(elt).css('z-index')) - 1);
+            }
+        });
+
+        this.window.css('z-index', '100');
+        this.openBtn.addClass('active');
+    }
+
+    open() {
+        this.window.show();
+        this.bringForward();
+        this.openBtn.addClass('active');
+        this.openBtn.addClass('open');
+    }
+
+    close() {
+        this.window.hide();
+        this.openBtn.removeClass('open');
+        this.openBtn.removeClass('active');
+    }
+
+    grab(e) {
+        // if the target is the element that has the event attach
+        if (e.target === e.currentTarget) {
+            // initial position of the mouse
+            this.pageX = e.pageX;
+            this.pageY = e.pageY;
+
+            this.content.on('mousemove', this.move.bind(this));
+        }
+    }
+
+    move(e) {
+        let valX = e.pageX - this.pageX;
+        let valY = e.pageY - this.pageY;
+
+        this.window.css('left', parseFloat(this.window.css('left')) + valX);
+        this.window.css('top', parseFloat(this.window.css('top')) + valY);
+        this.window.css('bottom', 'auto');
+        this.window.css('right', 'auto');
+
+        this.pageX = e.pageX;
+        this.pageY = e.pageY;
+
+        $(document).on('mouseup', () => {
+            this.content.off('mousemove');
+        });
+    }
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 $(function() {
+    let contentHeight = $('header.top').height() + $('footer.menu').height();
+    $('#content').css('height', `calc(100% - ${contentHeight}px)`);
+
+    $('.window').each((i, elt) => {
+        let windowClass = elt.className.replace('window', '');
+        let window = new WindowRPG(windowClass, 'content');
+    });
+
     let user = new User();
     let chat = new Chat('chat-form', 'chat-messages', 'user-message', 1);
 });
